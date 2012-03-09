f8ce4e3d408d9379bc25e11f4cabd505e5962b86
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2012-03-09 13:23:57 +0100

    more corrections to sigma_munu

--- SW.m
+++ SW.m
@@ -15,10 +15,11 @@ function SW()
   zeros(3) zeros(3) zeros(3) eye(3)-i*m*eye(3) ];
   
   % compute the SW term
+  % T_ee_cd = i/2 * sigma_munu * F_munu
   N = zeros(12);
   for mu = 0:3
     for nu = 0:3
-      S = i * 0.5 * Commutator( Gamma(mu), Gamma(nu) );
+      S = 0.5 * i * Commutator( Gamma(mu), Gamma(nu) );
     if(mu == nu)
       F = zeros(3);
     else
@@ -51,15 +52,15 @@ function SW()
     for nu = 0:3
       if( mu != nu )
         printf('BEGIN %d %d\n',mu,nu)
-        % i*sigma(mu,nu) (in spin)
-        S = 0.5 * i * Commutator( Gamma(mu), Gamma(nu) );
+        % sigma(mu,nu) (in spin)
+        S = i * 0.5 * Commutator( Gamma(mu), Gamma(nu) );
         % S external product 1_c
         A = [ eye(3)*S(1,1) eye(3)*S(1,2) eye(3)*S(1,3) eye(3)*S(1,4);
         eye(3)*S(2,1) eye(3)*S(2,2) eye(3)*S(2,3) eye(3)*S(2,4);
         eye(3)*S(3,1) eye(3)*S(3,2) eye(3)*S(3,3) eye(3)*S(3,4);
         eye(3)*S(4,1) eye(3)*S(4,2) eye(3)*S(4,3) eye(3)*S(4,4) ];
         % ( i sigma ( 1 +- i gamma5 + T_ee )^-1 )
-        R = A * M;
+        R = i * A * M;
         % Tr_Dirac R
         TR = R(1:3,1:3) + R(4:6,4:6) + R(7:9,7:9) + R(10:12,10:12)
         printf('END %d %d\n\n\n',mu,nu)
